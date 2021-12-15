866f03d927e4b3e08eb102b625d314327700d6dd
Zharkan <g.j@mailbox.org>
2021-12-15 15:41:12 +0100

    edit: to include imageIntro + button

--- components/home/intro.js
+++ components/home/intro.js
@@ -1,9 +1,17 @@
 import styles from "../../styles/Intro.module.css";
+import Button from "../common/button";
+import ImageIntro from "./imageIntro";
 
 export default function Intro() {
   return (
     <div className={styles.intro}>
-      <div></div>
+      <div className={styles.imageIntro}>
+        <ImageIntro />
+        
+      </div>
+      <div>
+      <Button link={"/discover"} linkText={"Découvrir"}/>
+      </div>
     </div>
   );
 }
