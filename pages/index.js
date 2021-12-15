5c7f47c2487bc3cdd608f3ad25545df9209d0496
Zharkan <g.j@mailbox.org>
2021-12-15 15:41:43 +0100

    edit: removed useless import

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Header from "../components/home/header";
 import Intro from "../components/home/intro";
 export default Home;
 
 function Home() {
   return (
     <div>
-      <Header />
       <Intro />
     </div>
   );
