4b5e8d02b058f2dac0b584f2d192d60b649e6372
Zharkan <g.j@mailbox.org>
2021-12-15 16:22:48 +0100

    edit: props to style button

--- components/home/intro.js
+++ components/home/intro.js
@@ -3,14 +3,17 @@ import Button from "../common/button";
 import ImageIntro from "./imageIntro";
 
 export default function Intro() {
+  const style = {
+    backgroundColor: "#ffdf43",
+    color: "#104798"
+  };
   return (
     <div className={styles.intro}>
       <div className={styles.imageIntro}>
         <ImageIntro />
-        
       </div>
       <div>
-      <Button link={"/discover"} linkText={"Découvrir"}/>
+        <Button link={"/discover"} style={style} linkText={"Découvrir"} />
       </div>
     </div>
   );
